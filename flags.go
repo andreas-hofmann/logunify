f62cec44def89149f6ed9a206780ad52e01f548a
Andreas Hofmann <mail@andreas-hofmann.org>
2021-07-28 22:09:42 +0200

    flags.go: Add some flags for remote connections.
    Signed-off-by: Andreas Hofmann <mail@andreas-hofmann.org>

--- flags.go
+++ flags.go
@@ -9,6 +9,10 @@ type Flags struct {
 	LogFileName    string
 	Replay         bool
 	Realtime       bool
+	Remote         string
+	Port           int
+	Listen         bool
+	Connect        bool
 }
 
 func ParseFlags() Flags {
@@ -18,8 +22,29 @@ func ParseFlags() Flags {
 	flag.StringVar(&f.LogFileName, "logfile", "logunify.log", "Log file to write to")
 	flag.BoolVar(&f.Replay, "replay", false, "Replay a stored log file")
 	flag.BoolVar(&f.Realtime, "realtime", false, "Replay a stored log file in real time")
+	flag.IntVar(&f.Port, "port", 20000, "Port to use when logging over TCP")
+	flag.BoolVar(&f.Listen, "listen", false, "Listen to incoming connections")
+	flag.StringVar(&f.Remote, "remote", "", "Remote to connect to")
 
 	flag.Parse()
+
+	f.Connect = false
+
+	if !f.Listen && len(f.Remote) <= 0 {
+		f.Port = 0
+	}
+
+	if f.Listen {
+		f.LogFileName = ""
+		f.Replay = true
+		f.Realtime = true
+		f.Connect = true
+	}
+
+	if len(f.Remote) > 0 {
+		f.Replay = false
+		f.Connect = true
+	}
 
 	return f
 }
