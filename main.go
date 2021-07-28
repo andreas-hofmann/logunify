de3489115df344b1adf0b76db859a5df72c88951
Andreas Hofmann <mail@andreas-hofmann.org>
2021-07-28 22:10:30 +0200

    main.go: Add remote handling to main function.
    Signed-off-by: Andreas Hofmann <mail@andreas-hofmann.org>

--- main.go
+++ main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"context"
+	"fmt"
 )
 
 func main() {
 	// First off, parse command line arguments
 	flags := ParseFlags()
 	cfg := ReadConfig(flags)
-	logging := InitLogfile(flags.LogFileName)
+
+	logging := NewLogFile(flags.LogFileName, !flags.Replay)
 	defer logging.Close()
+
+	peer := ""
+	if flags.Connect {
+		peer = flags.Remote + ":" + fmt.Sprint(flags.Port)
+	}
+
+	remote := NewLogRemote(peer, !flags.Replay)
+	defer remote.Close()
 
 	// Set up a context, to allwo terminating background processes
 	ctx := context.Background()
@@ -29,6 +39,7 @@ func main() {
 		}
 	} else {
 		logging.Replay(logchan, flags.Realtime)
+		remote.Replay(logchan, flags.Realtime)
 	}
 
 	// Receive log data in the background and send it to logfile + views
@@ -36,6 +47,7 @@ func main() {
 		for l := range logchan {
 			if !flags.Replay {
 				logging.Write(l)
+				remote.Write(l)
 			}
 
 			ui.AddData(l)
