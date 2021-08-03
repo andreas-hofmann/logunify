3138f8eb611465d608892893b89c61954b30d4f5
Andreas Hofmann <git@andreas-hofmann.org>
2021-08-03 15:12:57 +0200

    build.sh: Build for windows + android as well.
    Signed-off-by: Andreas Hofmann <git@andreas-hofmann.org>

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 VERSION=`git describe --all --long --dirty`
+
+export GOOS=linux GOARCH=amd64
 go build -ldflags "-X main.version=$VERSION"
+
+export GOOS=windows GOARCH=amd64
+go build -ldflags "-X main.version=$VERSION"
+
+export GOOS=android GOARCH=arm64
+go build -ldflags "-X main.version=$VERSION" -o logunify_android
